1feb2585146a6cb3aae32948d881e80d5c93a2de
SC-V <37342773+SC-V@users.noreply.github.com>
2023-01-14 15:18:36 -0600

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -250,6 +250,7 @@ st.download_button(
 )
 
 with st.expander("Orders on a map:"):
+    st.caption(f'Hower order to see details. :green[Green] orders are delivered, and :red[red] – are the in delivery, returning or canncelled state')
     chart_data_delivered = filtered_frame[filtered_frame["status"].isin(['delivered', 'delivered_finish'])]
     chart_data_in_delivery = filtered_frame[~filtered_frame["status"].isin(['delivered', 'delivered_finish'])]
     view_state_lat = filtered_frame['lat'].iloc[0]
