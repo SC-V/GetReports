84d4a6541151fb8e83a5b8759855bebb50364f71
SC-V <37342773+SC-V@users.noreply.github.com>
2023-01-15 15:17:32 -0600

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -308,7 +308,7 @@ with st.expander("Orders on a map:"):
                 'TextLayer',
                 data=stores_on_a_map,
                 get_position='[store_lon, store_lat]',
-                get_text='[cutoff, store_name]'
+                get_text='[cutoff, store_name]',
                 get_color='[0, 0, 0]',
                 get_size=12,
                 get_pixel_offset='[0, 15]',
