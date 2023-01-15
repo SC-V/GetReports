e54bff80fb98ec533f8a6d05b42d47cdccdd52f5
SC-V <37342773+SC-V@users.noreply.github.com>
2023-01-15 15:09:17 -0600

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -268,6 +268,7 @@ with st.expander("Orders on a map:"):
     view_state_lat = filtered_frame['lat'].iloc[0]
     view_state_lon = filtered_frame['lon'].iloc[0]
     stores_on_a_map = filtered_frame.groupby(['store_name', 'store_lon', 'store_lat'])['cutoff'].transform(lambda x: ', '.join(x))
+    st.dataframe.stores_on_a_map
     st.pydeck_chart(pdk.Deck(
         map_style=None,
         initial_view_state=pdk.ViewState(
