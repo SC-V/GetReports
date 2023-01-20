1a92f0a63486e4a37f76557f0783f13c9a4c5f01
SC-V <37342773+SC-V@users.noreply.github.com>
2023-01-20 13:26:44 -0600

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -30,6 +30,14 @@ SECRETS_MAP = {"Petco": 0,
                "Ceviz Agaci": 12,
                "Guven Sanat": 13}
 
+
+def calculate_distance(row):
+    location_1 = (row["lat"], row["lon"])
+    location_2 = (row["store_lat"], row["store_lon"])
+    row["linear_distance"] = round(hs.haversine(location_1, location_2), 2)
+    return row
+
+
 def get_pod_orders():
     service = discovery.build('sheets', 'v4', discoveryServiceUrl=
     'https://sheets.googleapis.com/$discovery/rest?version=v4',
@@ -55,9 +63,48 @@ def check_for_pod(row, orders_with_pod):
     return row
 
 
+def get_cod_orders():
+    service = discovery.build('sheets', 'v4', discoveryServiceUrl=
+    'https://sheets.googleapis.com/$discovery/rest?version=v4',
+                              developerKey=COD_SHEET_KEY)
+    spreadsheet_id = COD_SHEET_ID
+
+    range_ = 'C:C'
+    request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+    response = request.execute()
+    cod_orders = [item for sublist in response["values"] for item in sublist]
+    cod_orders = [item.replace(' ', '').replace('TRK', '') for item in cod_orders]
+
+    range_ = 'E:E'
+    request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+    response = request.execute()
+    cod_links = [item for sublist in response["values"] for item in sublist]
+
+    orders_with_links = dict(zip(cod_orders, cod_links))
+    return orders_with_links
+
+
+def check_for_cod(row, orders_with_cod: dict):
+    if row["price_of_goods"] < 1:
+        row["cash_collected"] = "Prepaid"
+        row["cash_prooflink"] = "Prepaid"
+        return row
+    if row["status"] not in ["delivered", "delivered_finish"]:
+        row["cash_collected"] = "-"
+        row["cash_prooflink"] = "-"
+        return row
+    if str(row["client_id"]) in orders_with_cod.keys():
+        row["cash_collected"] = "Deposit verified"
+        row["cash_prooflink"] = orders_with_cod[row["client_id"]]
+    else:
+        row["cash_collected"] = "Not verified"
+        row["cash_prooflink"] = "No link"
+    return row
+
+
 def get_claims(date_from, date_to, cursor=0):
     url = API_URL
-    
+
     timezone_offset = "+03:00" if SECRETS_MAP[selected_client] in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] else "-06:00"
     payload = json.dumps({
         "created_from": f"{date_from}T00:00:00{timezone_offset}",
@@ -65,9 +112,9 @@ def get_claims(date_from, date_to, cursor=0):
         "limit": 1000,
         "cursor": cursor
     }) if cursor == 0 else json.dumps({"cursor": cursor})
-    
+
     client_secret = CLAIM_SECRETS[SECRETS_MAP[selected_client]]
-    
+
     headers = {
         'Content-Type': 'application/json',
         'Accept-Language': 'en',
@@ -86,15 +133,15 @@ def get_claims(date_from, date_to, cursor=0):
 
 
 def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
-
     offset_back = 0
     if option == "Yesterday":
         offset_back = 1
     elif option == "Tomorrow":
         offset_back = -1
-    
-    client_timezone = "Europe/Istanbul" if SECRETS_MAP[selected_client] in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] else "America/Mexico_City"
-    
+
+    client_timezone = "Europe/Istanbul" if SECRETS_MAP[selected_client] in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                                                                            13] else "America/Mexico_City"
+
     if not start_:
         today = datetime.datetime.now(timezone(client_timezone)) - datetime.timedelta(days=offset_back)
         search_from = today.replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=3)
@@ -102,7 +149,7 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
         date_from = search_from.strftime("%Y-%m-%d")
         date_to = search_to.strftime("%Y-%m-%d")
     else:
-        today = datetime.datetime.now(timezone(client_timezone)) - datetime.timedelta(days=offset_back)
+        today = datetime.datetime.now(timezone(client_timezone))
         date_from_offset = datetime.datetime.fromisoformat(start_).astimezone(
             timezone(client_timezone)) - datetime.timedelta(days=2)
         date_from = date_from_offset.strftime("%Y-%m-%d")
@@ -158,11 +205,18 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
             report_route_id = claim['route_id']
         except:
             report_route_id = "No route"
+        try:
+            report_price_of_goods = 0
+            report_goods = claim['items']
+            for item in claim['items']:
+                report_price_of_goods += float(item['cost_value'])
+        except:
+            report_price_of_goods = 0
         row = [report_cutoff, report_client_id, report_claim_id, report_pod_point_id,
                report_pickup_address, report_receiver_address, report_receiver_phone, report_receiver_name,
                report_status, report_status_time, report_store_name, report_courier_name, report_courier_park,
                report_return_reason, report_return_comment, report_autocancel_reason, report_route_id,
-               report_longitude, report_latitude, report_store_longitude, report_store_latitude]
+               report_longitude, report_latitude, report_store_longitude, report_store_latitude, report_price_of_goods]
         report.append(row)
 
     result_frame = pandas.DataFrame(report,
@@ -171,11 +225,19 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
                                              "receiver_name", "status", "status_time",
                                              "store_name", "courier_name", "courier_park",
                                              "return_reason", "return_comment", "cancel_comment",
-                                             "route_id", "lon", "lat", "store_lon", "store_lat"])
+                                             "route_id", "lon", "lat", "store_lon", "store_lat", "price_of_goods"])
     orders_with_pod = get_pod_orders()
+    result_frame = result_frame.apply(lambda row: calculate_distance(row), axis=1)
     result_frame = result_frame.apply(lambda row: check_for_pod(row, orders_with_pod), axis=1)
+    orders_with_cod = get_cod_orders()
     result_frame.insert(3, 'proof', result_frame.pop('proof'))
+    if selected_client in ["Quiken"]:
+        result_frame = result_frame.apply(lambda row: check_for_cod(row, orders_with_cod), axis=1)
+        result_frame.insert(4, 'cash_collected', result_frame.pop('cash_collected'))
+        result_frame.insert(5, 'cash_prooflink', result_frame.pop('cash_prooflink'))
+        result_frame.insert(6, 'price_of_goods', result_frame.pop('price_of_goods'))
     return result_frame
+
 
 streamlit_analytics.start_tracking()
 st.markdown(f"# Routes report")
@@ -186,27 +248,35 @@ st.sidebar.caption(f"Page reload doesn't refresh the data.\nInstead, use this bu
 
 selected_client = st.sidebar.selectbox(
     "Select client:",
-    ["Petco", "Quiken", "Cubbo", "Lens Market", "Ebebek", "Supplementer", "Sadece-eczane", "Osevio Internet Hizmetleri", "Mevsimi", "Candy Gift", "Akel", "Espresso Perfetto", "Ceviz Agaci", "Guven Sanat"]
+    ["Petco", "Quiken", "Cubbo", "Lens Market", "Ebebek", "Supplementer", "Sadece-eczane", "Osevio Internet Hizmetleri",
+     "Mevsimi", "Candy Gift", "Akel", "Espresso Perfetto", "Ceviz Agaci", "Guven Sanat"]
 )
 
 option = st.sidebar.selectbox(
     "Select report date:",
-    ["Today", "Yesterday", "Tomorrow"]
+    ["Today", "Yesterday", "Tomorrow", "Monthly"]
 )
+
 
 @st.experimental_memo
 def get_cached_report(period):
-    report = get_report(period)
+
+    if option == "Monthly":
+        report = get_report(period, start_="2022-12-31", end_="2023-01-31")
+    else:
+        report = get_report(period)
     df_rnt = report[report['status'] != 'cancelled']
     df_rnt = df_rnt.groupby(['courier_name', 'route_id', 'store_name'])['pickup_address'].nunique().reset_index()
     routes_not_taken = df_rnt[(df_rnt['courier_name'] == "No courier yet") & (df_rnt['route_id'] != "No route")]
     try:
-        pod_provision_rate = len(report[report['proof'] == "Proof provided"]) / len(report[report['status'].isin(['delivered', 'delivered_finish'])])
+        pod_provision_rate = len(report[report['proof'] == "Proof provided"]) / len(
+            report[report['status'].isin(['delivered', 'delivered_finish'])])
         pod_provision_rate = f"{pod_provision_rate:.0%}"
     except:
         pod_provision_rate = "--"
     delivered_today = len(report[report['status'].isin(['delivered', 'delivered_finish'])])
     return report, routes_not_taken, pod_provision_rate, delivered_today
+
 
 df, routes_not_taken, pod_provision_rate, delivered_today = get_cached_report(option)
 
@@ -230,7 +300,6 @@ statuses = st.sidebar.multiselect(
      'new',
      'pickup_arrived'])
 
-
 stores = st.sidebar.multiselect(
     "Filter by stores:",
     df["store_name"].unique()
@@ -239,7 +308,7 @@ stores = st.sidebar.multiselect(
 only_no_proofs = st.sidebar.checkbox("Only parcels without proofs")
 
 if only_no_proofs:
-  df = df[df["proof"] == "No proof"]
+    df = df[df["proof"] == "No proof"]
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Not pickuped routes :minibus:", str(len(routes_not_taken)))
@@ -257,13 +326,15 @@ else:
 
 st.dataframe(filtered_frame)
 
-client_timezone = "Europe/Istanbul" if SECRETS_MAP[selected_client] in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] else "America/Mexico_City"
+client_timezone = "Europe/Istanbul" if SECRETS_MAP[selected_client] in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                                                                        13] else "America/Mexico_City"
 TODAY = datetime.datetime.now(timezone(client_timezone)).strftime("%Y-%m-%d") \
     if option == "Today" \
     else datetime.datetime.now(timezone(client_timezone)) - datetime.timedelta(days=1)
 
 stores_with_not_taken_routes = ', '.join(str(x) for x in routes_not_taken["store_name"].unique())
-st.caption(f'Total of :blue[{len(filtered_frame)}] orders in the table. Following stores have not pickuped routes: :red[{stores_with_not_taken_routes}]')
+st.caption(
+    f'Total of :blue[{len(filtered_frame)}] orders in the table. Following stores have not pickuped routes: :red[{stores_with_not_taken_routes}]')
 
 with pandas.ExcelWriter(FILE_BUFFER, engine='xlsxwriter') as writer:
     df.to_excel(writer, sheet_name='routes_report')
@@ -277,15 +348,20 @@ with pandas.ExcelWriter(FILE_BUFFER, engine='xlsxwriter') as writer:
     )
 
 with st.expander(":round_pushpin: Orders on a map:"):
-    st.caption(f'Hover order to see details. Stores are the big points on a map. :green[Green] orders are delivered, and :red[red] – are the in delivery state. :orange[Orange] are returned or returning. Gray are cancelled.')
+    st.caption(
+        f'Hover order to see details. Stores are the big points on a map. :green[Green] orders are delivered, and :red[red] – are the in delivery state. :orange[Orange] are returned or returning. Gray are cancelled.')
     chart_data_delivered = filtered_frame[filtered_frame["status"].isin(['delivered', 'delivered_finish'])]
-    chart_data_in_delivery = filtered_frame[~filtered_frame["status"].isin(['delivered', 'delivered_finish', 'cancelled', 'cancelled_by_taxi', 'returning', 'returned_finish', 'return_arrived'])]
-    chart_data_returns = filtered_frame[filtered_frame["status"].isin(['returning', 'returned_finish', 'return_arrived'])]
+    chart_data_in_delivery = filtered_frame[~filtered_frame["status"].isin(
+        ['delivered', 'delivered_finish', 'cancelled', 'cancelled_by_taxi', 'returning', 'returned_finish',
+         'return_arrived'])]
+    chart_data_returns = filtered_frame[
+        filtered_frame["status"].isin(['returning', 'returned_finish', 'return_arrived'])]
     chart_data_cancels = filtered_frame[filtered_frame["status"].isin(['cancelled', 'cancelled_by_taxi'])]
     view_state_lat = filtered_frame['lat'].iloc[0]
     view_state_lon = filtered_frame['lon'].iloc[0]
     filtered_frame['cutoff'] = filtered_frame['cutoff'].str.split(' ').str[1]
-    stores_on_a_map = filtered_frame.groupby(['store_name', 'store_lon', 'store_lat'])['cutoff'].agg(lambda x: ', '.join(x.unique())).reset_index(drop=False)
+    stores_on_a_map = filtered_frame.groupby(['store_name', 'store_lon', 'store_lat'])['cutoff'].agg(
+        lambda x: ', '.join(x.unique())).reset_index(drop=False)
     stores_on_a_map.columns = ['store_name', 'store_lon', 'store_lat', 'cutoff']
     st.pydeck_chart(pdk.Deck(
         map_style=None,
