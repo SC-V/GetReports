9f7b036b5bfc4b68a9b4c6ec9dcdd80b5e7521a8
SC-V <37342773+SC-V@users.noreply.github.com>
2023-03-23 00:44:05 -0300

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -141,7 +141,8 @@ def check_for_lateness(row):
         cutoff_time = datetime.datetime.strptime(f"{datetime.datetime.today().strftime('%Y-%m-%d') - datetime.timedelta(days=1)} {row['cutoff']}", "%Y-%m-%d %H:%M")
         print(cutoff_time)
     current_time = datetime.datetime.now(timezone(client_timezone))
-    difference = (current_time.astimezone(timezone(client_timezone)) - cutoff_time.astimezone(timezone(client_timezone))).seconds / 60
+    difference = (current_time.astimezone(timezone(client_timezone)) - cutoff_time.astimezone(timezone(client_timezone))).seconds
+    difference = divmod(difference.days * 24 * 60 * 60 + difference.seconds, 60)
     row['diff_min'] = difference
     st.write(f"{cutoff_time} | {current_time} | {difference}")
     return row
