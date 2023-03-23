78232fef7ee860f49c138e723b7adc2e3647981d
SC-V <37342773+SC-V@users.noreply.github.com>
2023-03-23 00:32:22 -0300

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -134,8 +134,11 @@ def check_for_cod(row, orders_with_cod: dict):
 
   
 def check_for_lateness(row):
-    cutoff_time = datetime.datetime.strptime(row['cutoff'], "%H:%M")
-    current_time = datetime.datetime.now()
+    if option == "Today":
+        cutoff_time = datetime.datetime.strptime(f"{datetime.today().strftime('%Y-%m-%d')} {row['cutoff']}", "%Y-%m-%d %H:%M") 
+    elif option == "Yesterday":
+        cutoff_time = datetime.datetime.strptime(f"{datetime.today().strftime('%Y-%m-%d') - datetime.timedelta(days=1)} {row['cutoff']}", "%Y-%m-%d %H:%M")
+    current_time = datetime.datetime.now(timezone(client_timezone))
     difference = (current_time - cutoff_time).seconds / 60
     row['diff_min'] = difference
     st.write(f"{cutoff_time} | {current_time} | {difference}")
