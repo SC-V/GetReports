fe725c334d0f92b5a35c49cf8f3f7d95a7a4fd31
SC-V <37342773+SC-V@users.noreply.github.com>
2023-03-23 09:57:54 -0300

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -145,10 +145,12 @@ def check_for_lateness(row):
     elif option == "Yesterday":
         difference_munutes = 999  # magic number that is >30
     try:
-        if (row["1. created"] not in ["-", 1] or row["2. assigned"] not in ["-", 1] or row["3. pickuped"] not in ["-", 1]) and option == "Yesterday":
-            row["cutoff"] = row["cutoff"] + "🙀"
+        if (row["2. assigned"] not in ["-", 1] or row["3. pickuped"] not in ["-", 1]) and option == "Yesterday":
+            row["cutoff"] = row["cutoff"] + " 🙀🙀🙀"
         elif (row["1. created"] not in ["-", 1] or row["2. assigned"] not in ["-", 1] or row["3. pickuped"] not in ["-", 1]) and option == "Today" and difference_munutes >= 30:
-            row["cutoff"] = row["cutoff"] + "🙀"
+            row["cutoff"] = row["cutoff"] + " 🙀"
+        elif (row["1. created"] not in ["-", 1] or row["2. assigned"] not in ["-", 1] or row["3. pickuped"] not in ["-", 1]) and option == "Today" and difference_munutes >= 10:
+            row["cutoff"] = row["cutoff"] + " 🙀🙀🙀"
     except:
         print("No warnings")
     return row
